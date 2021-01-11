f8a296ffaa860394f62bb93e58940e31ea9124d4
Renan Santos <renanfilipe10@hotmail.com>
2021-01-11 08:34:32 -0300

    feat: add app router

---
+++ src/presentation/components/router/router.tsx
@@ -0,0 +1,18 @@
+import '@/presentation/styles/global.scss'
+
+import React from 'react'
+import { BrowserRouter, Route, Switch } from 'react-router-dom'
+
+import { Login } from '@/presentation/pages'
+
+const Router: React.FC = () => {
+  return (
+    <BrowserRouter>
+      <Switch>
+        <Route path="/login" exact component={Login} />
+      </Switch>
+    </BrowserRouter>
+  )
+}
+
+export default Router
